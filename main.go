8c354e7eceba2643b57caa447e0401f9a7a56e53
kitt3911 <ivanignatenko28@gmail.com>
2021-09-22 22:41:05 +0600

    refactor: file structure fixed

--- main.go
+++ main.go
@@ -1,12 +1,12 @@
 package main
 
 import (
-	"github.com/MartIden/deep-throtle-parser/command"
-	"github.com/PuerkitoBio/goquery"
 	"log"
-	"net/http"
 	"os"
-	"regexp"
+
+	"github.com/MartIden/deep-throtle-parser/command"
+	"github.com/MartIden/deep-throtle-parser/parser"
+	"github.com/MartIden/deep-throtle-parser/request"
 )
 
 func main() {
@@ -20,20 +20,23 @@ func main() {
 	var linksForParsing [][]string
 	linksForParsing = append(linksForParsing, []string{args.Url})
 
-	for i := 0; i < int(args.Deep); i++ {
+	for _, links := range linksForParsing {
 		var currentLinks []string
-		for _, link := range linksForParsing[i] {
-			resp, err := getPage(link)
+		
+		for _, link := range links {
+			resp, err := request.GetPageByUri(link)
+
+			template := resp.Body
 			if err != nil {
 				log.Println(err.Error())
 				os.Exit(1)
 			}
-			pageLinks, err := getInnerLinks(resp, args.Url)
+			links, err := parser.GetInnerLinks(template, args.Url)
 			if err != nil {
 				log.Println(err.Error())
 				os.Exit(1)
 			}
-			for _, currentLink := range pageLinks {
+			for _, currentLink := range links {
 				currentLinks = append(currentLinks, currentLink)
 			}
 		}
@@ -47,33 +50,4 @@ func main() {
 			listOfLinks = append(listOfLinks, link)
 		}
 	}
-
-}
-
-func getInnerLinks(resp *http.Response, baseLink string) ([]string, error) {
-
-	doc, err := goquery.NewDocumentFromReader(resp.Body)
-	var results []string
-	if err != nil {
-		return nil, err
-	}
-	doc.Find("a").Each(func(i int, s *goquery.Selection) {
-
-		href, _ := s.Attr("href")
-		isMatch, _ := regexp.MatchString(baseLink, href)
-		if isMatch {
-			results = append(results, href)
-		}
-	})
-	defer resp.Body.Close()
-	return results, nil
-}
-
-func getPage(uri string) (*http.Response, error) {
-	client := http.Client{}
-	resp, err := client.Get(uri)
-	if err != nil {
-		return resp, err
-	}
-	return resp, nil
 }
