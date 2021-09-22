44bdd3bac4499fbfb7df86345f67d26a1407c93b
kitt3911 <ivanignatenko28@gmail.com>
2021-09-22 22:41:05 +0600

    refactor: file structure fixed

--- main.go
+++ main.go
@@ -2,12 +2,12 @@ package main
 
 import (
 	"fmt"
-	"github.com/MartIden/deep-throtle-parser/command"
-	"github.com/PuerkitoBio/goquery"
 	"log"
-	"net/http"
 	"os"
-	"regexp"
+
+	"github.com/MartIden/deep-throtle-parser/command"
+	"github.com/MartIden/deep-throtle-parser/parser"
+	"github.com/MartIden/deep-throtle-parser/request"
 )
 
 func main() {
@@ -23,12 +23,14 @@ func main() {
 
 	for _, links := range linksForParsing {
 		for _, link := range links {
-			resp, err := getPage(link)
+			resp, err := request.GetPageByUri(link)
+
+			template := resp.Body
 			if err != nil {
 				log.Println(err.Error())
 				os.Exit(1)
 			}
-			links, err := getInnerLinks(resp, args.Url)
+			links, err := parser.GetInnerLinks(template, args.Url)
 			if err != nil {
 				log.Println(err.Error())
 				os.Exit(1)
@@ -39,32 +41,4 @@ func main() {
 		}
 
 	}
-}
-
-func getInnerLinks(resp *http.Response, baseLink string) ([]string, error) {
-
-	doc, err := goquery.NewDocumentFromReader(resp.Body)
-	var results []string
-	if err != nil {
-		return nil, err
-	}
-	doc.Find("a").Each(func(i int, s *goquery.Selection) {
-
-		href, _ := s.Attr("href")
-		isMatch, _ := regexp.MatchString(baseLink, href)
-		if isMatch {
-			results = append(results, href)
-		}
-	})
-	defer resp.Body.Close()
-	return results, nil
-}
-
-func getPage(uri string) (*http.Response, error) {
-	client := http.Client{}
-	resp, err := client.Get(uri)
-	if err != nil {
-		return resp, err
-	}
-	return resp, nil
 }
